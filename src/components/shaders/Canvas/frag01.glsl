67de772c53c1a112e9565d60ba5ed6e39fa7e890
shuta13 <hollystarsun@gmail.com>
2020-02-09 01:21:55 +0900

    wip: キモい

--- src/components/shaders/Canvas/frag01.glsl
+++ src/components/shaders/Canvas/frag01.glsl
@@ -4,7 +4,7 @@ uniform float t;       // time
 uniform vec2  r;       // resolution
 
 void main(void){
-    vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
+    vec2 p = (gl_FragCoord.xy * 10.0 - r) / min(r.x, r.y);
     float l = length(vec2(0, sin(p.y + m.y * 0.01) + sin(p.x - m.x * 0.01)));
     gl_FragColor = vec4(vec3(l), 1.0);
 }

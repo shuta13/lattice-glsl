fe20a426643d59be1a94b1fc3a423b13f9549919
shuta13 <hollystarsun@gmail.com>
2020-02-09 01:12:37 +0900

    wip

--- src/components/shaders/Canvas/frag01.glsl
+++ src/components/shaders/Canvas/frag01.glsl
@@ -8,6 +8,6 @@ const float PI2 = PI* 2.;
 
 void main(void){
     vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
-    float l = length(vec2( 0, p.y + sin(p.x + t)));
+    float l = length(vec2( 0, sin(p.y + t) + sin(p.x + t)));
     gl_FragColor = vec4(vec3(l), 1.0);
 }

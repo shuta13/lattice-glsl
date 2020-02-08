1cf0297647fa958a82772c44bce705fa4d2a60cd
shuta13 <hollystarsun@gmail.com>
2020-02-09 01:09:02 +0900

    wip

--- src/components/shaders/Canvas/frag01.glsl
+++ src/components/shaders/Canvas/frag01.glsl
@@ -1,5 +1,5 @@
 precision mediump float;
-uniform vec2  m;       // mouse
+// uniform vec2  m;       // mouse
 uniform float t;       // time
 uniform vec2  r;       // resolution
 
@@ -8,6 +8,6 @@ const float PI2 = PI* 2.;
 
 void main(void){
     vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
-    float l = length(vec2( 0, p.y + sin(p.x * cos(t) * sin(t))));
+    float l = length(vec2( 0, p.y + sin(p.x + t)));
     gl_FragColor = vec4(vec3(l), 1.0);
 }
